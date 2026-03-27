de15bef7fbcf9dfcda68d5f588779cc7cf9d2080
Gustavo A. López Perea <gustavoalvearlopezperea@gmail.com>
2026-03-28 00:15:17 +0100

    Cambiando el nombre y apellido

--- codigo.py
+++ codigo.py
@@ -1,5 +1,5 @@
-nombre = "GusiTherRial"
-apellido = "Perez"
+nombre = "Gusi"
+apellido = "TherRial"
 
 saludo = "Hola, mi nombre es " + nombre + " " + apellido + " y estoy aprendiendo GitHub."
 print(saludo)
